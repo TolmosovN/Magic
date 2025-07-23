45ce7a3101cf1182f6bf19e9b08d3768a2d5b031
ZhurilkinS <slava-7.10@mail.ru>
2025-07-23 20:08:49 +0300

    routing client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-
-
-
+import { Route, Routes, useNavigate } from "react-router";
+import axios from "axios";
 
 function App() {
- 
-
   return (
-    <>
-      Hello
-    </>
-  )
+    <Routes>
+      <Route
+        element={<Layout user={user}  />}
+      ></Route>
+      <Route path="/profile" element={<ProfilePage user={user} />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/register" element={<SignupPage />} />
+      <Route
+        path="/signin"
+        element={<SignInPage  />}
+      />
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
