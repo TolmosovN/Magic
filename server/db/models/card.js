e940d5bdf3f2cfaa6f76b4fefcf001376182dec1
ZhurilkinS <slava-7.10@mail.ru>
2025-07-23 21:47:14 +0300

    db reset

--- server/db/models/card.js
+++ server/db/models/card.js
@@ -8,8 +8,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User}) {
-      this.belongsToMany(User, { foreignKey:"userId"})
+    static associate({User,Cart}) {
+      this.hasMany(Cart, { foreignKey:"cardId"})
+      this.belongsTo(User, { 
+        foreignKey:"userId",
+        as: 'seller',
+      })
     }
   }
   Card.init({
